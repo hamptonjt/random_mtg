60f81b3a88eaed2dda5ff8a23e564b889160b67c
Jerry Hampton <hamptonjt@hotmail.com>
2019-02-10 13:47:43 -0500

    First twitter attempt

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-const mtg = require('mtgsdk')
+'use strict'
+let Twitter = require('twitter')
+let request = require('request')
 
-const randNum = Math.floor((Math.random() * 5000) + 1) 
+const randomUrl = 'https://api.scryfall.com/cards/random'
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-//console.log('Random Number generated:', randNum)
-readline.question('Do you wish to search for a card or set? ', (searchVal) => {
-    if (searchVal.toLowerCase() === 'card') {
-        readline.question('What is the name of the card? ', (cardSearch) => {
-            mtg.card.where({name: cardSearch}).then(cards => {
-                console.log(cards.length)
-                for (var card of cards) {
-                    console.log(card)
-                }
-                readline.close()
-            })
-        })
-    } else if (searchVal.toLowerCase() === 'set') {
-        readline.question('What is the name of the set? ', (setSearch) => {
-            mtg.set.where({name: setSearch}).then(sets => {
-                console.log(sets.length)
-                for (var set of sets) {
-                    console.log(set)
-                }
-                readline.close()
-            })
-        })
+/**
+ * @param context {WebtaskContext}
+ */
+module.exports = function (context, cb) {
+  request({method: 'GET', url: randomUrl, json: true}, function (e, response, body) {
+    if (e) {
+      console.error(e)
+      cb(error)
     }
-})
+    let card = body
+    // console.log(card.name, '-', card.set_name)
+    // console.log('Release Date:', card.released_at)
+    // console.log(card.image_uris.normal)
+
+    let client = new Twitter(context.secrets)
+    let tweet = card.name + ' - ' + card.set_name + '\nRelease Date: ' + card.released_at
+    request.get({url: card.image_uris.normal, encoding: null}, function (err, response, body) {
+      if (!err && response.statusCode === 200) {
+        client.post('media/upload', {media: body}, function (error, media, response) {
+          if (error) {
+            console.error('Error from media/upload: ' + error)
+            cb(error)
+          }
+
+          console.log('Image uploaded to Twitter')
+
+          var status = {
+            status: tweet,
+            media_ids: media.media_id_string
+          }
+
+          client.post('statuses/update', status, function (error, tweet, response) {
+            if (!error) {
+              console.log('Tweeted OK!')
+            }
+          })
+        }) 
+      }
+    })
+  })
+  cb(null, {status: 'Ok'})
+}
